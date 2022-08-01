56b7a76c0b8040c929580939120590ef41103914
fumtas1k <fumta01a7@gmail.com>
2022-08-01 11:16:42 +0900

    add:vuexによるeventsデータ管理

--- frontend/src/components/Calendar.vue
+++ frontend/src/components/Calendar.vue
@@ -1,33 +1,28 @@
 <template lang="pug">
 div
   h1 Calendar
-  p events:
-  p {{ events }}
+  ul
+    li(v-for="event in events" :key="event.id") {{ event.name }}
   button(type="submit" @click="fetchEvents()") fetchEvents
+
+  CalendarDetails
 </template>
 
 <script>
-import axios from "axios";
+import { mapGetters, mapActions } from "vuex";
+import CalendarDetails from "./CalendarDetails.vue";
 
 export default {
   // eslint-disable-next-line vue/multi-word-component-names
   name: "Calendar",
-  data() {
-    return {
-      events: [],
-    };
+  components: {
+    CalendarDetails,
+  },
+  computed: {
+    ...mapGetters("events", ["events"]),
   },
   methods: {
-    fetchEvents() {
-      axios
-        .get("http://localhost:3000/events")
-        .then((response) => {
-          this.events = response.data;
-        })
-        .catch((error) => {
-          console.error(error);
-        });
-    },
+    ...mapActions("events", ["fetchEvents"]),
   },
 };
 </script>
